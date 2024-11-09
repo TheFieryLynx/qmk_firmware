481cc9dce07ad2f1216b156961b7ae7bac46be39
Andrew Biryukov <biryukov.andrej2000@mail.ru>
2024-11-09 14:03:30 +0300

    fully working my keymap

--- keyboards/crkbd/keymaps/TheFieryLynx/keymap.c
+++ keyboards/crkbd/keymaps/TheFieryLynx/keymap.c
@@ -17,8 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-
-#include "eeconfig.h"
+#include "../ruen.h"
 
 #define WD_LEFT     LCTL(KC_LEFT) // move window left
 #define WD_RGHT     LCTL(KC_RIGHT) // move window right
@@ -27,75 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LC_SC       LGUI(KC_L) // lock screen
 #define OP_TM       LGUI(KC_T) // open terminal
 #define CL_WD       LGUI(KC_W) // close window
-#define CH_LG       LALT(KC_CAPS) // change language
-
-// CM_ == Custom_ - layer language independent symbols
-
-enum custom_keycodes {
-    LG_ACTN = SAFE_RANGE, // change language and change flag
-    LG_SYNC, // change flag if system language is out of sync
-    CM_LCBR, // {
-    CM_RCBR, // }
-    CM_LBRC, // [
-    CM_RBRC, // ]
-};
-
-typedef union {
-  uint32_t raw;
-  struct {
-    bool language_state :1; // true - US, false - RU
-  };
-} user_config_t;
-
-user_config_t user_config;
-
-void keyboard_post_init_user(void) {
-  user_config.raw = eeconfig_read_user();
-}
-
-void change_eeconfig(void) {
-    user_config.language_state = !user_config.language_state;
-    eeconfig_update_user(user_config.raw);
-}
-
-void tap_symbol(uint16_t symbol) {
-    if (user_config.language_state) {
-        tap_code16(symbol);
-    } else {
-        tap_code16(CH_LG);
-        tap_code16(symbol);
-        tap_code16(CH_LG);
-    }
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        switch (keycode) {
-            case LG_ACTN:
-                change_eeconfig();
-                tap_code16(CH_LG);
-                return false;
-            case LG_SYNC:
-                change_eeconfig();
-                return false;
-            case CM_LCBR:
-                tap_symbol(KC_LCBR);
-                return false;
-            case CM_RCBR:
-                tap_symbol(KC_RCBR);
-                return false;
-            case CM_LBRC:
-                tap_symbol(KC_LBRC);
-                return false;
-            case CM_RBRC:
-                tap_symbol(KC_RBRC);
-                return false;
-            default:
-                break;
-        }
-    }
-    return true;
-}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
@@ -115,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_GRV, XXXXXXX, XXXXXXX,   KC_UP, XXXXXXX, SC_AREA,                      LG_SYNC, KC_LPRN, KC_RPRN, XXXXXXX, KC_MINS, KC_LBRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,KC_RIGHT,   SC_WD,                      XXXXXXX, CM_LCBR, CM_RCBR,   LC_SC,  KC_EQL, KC_QUOT,
+      XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN,KC_RIGHT,   SC_WD,                      XXXXXXX, RN_LCBR, RN_RCBR,   LC_SC,  KC_EQL, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, WD_LEFT, KC_CAPS, WD_RGHT, XXXXXXX,                      LG_ACTN, CM_LBRC, CM_RBRC, XXXXXXX, KC_BSLS, KC_RBRC,
+      KC_LSFT, XXXXXXX, WD_LEFT, KC_CAPS, WD_RGHT, XXXXXXX,                      LG_ACTN, RN_LBRC, RN_RBRC, XXXXXXX, KC_BSLS, KC_RBRC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______, _______,    _______,   MO(3), KC_RALT
                                       //`--------------------------'  `--------------------------'
